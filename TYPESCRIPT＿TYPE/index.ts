142bee865128aedc15b8cf86dd5a5fc5806c24a1
Charlie <158181191+Charlie2222223@users.noreply.github.com>
2024-10-22 13:50:18 +0900

    代表的な方の勉強

---
+++ TYPESCRIPT＿TYPE/index.ts
@@ -0,0 +1,17 @@
+let hasValue: boolean = true;
+
+/**
+ * number型は小数点でもマイナスでも入れることができる
+ */
+let count: number = 10;
+let float: number = 3.14;
+let negative: number = -0.12;
+
+/**
+ * stringはシングルでもダブルでもバックでも可能
+ */
+let single: string = 'hello';
+let double: string = "world";
+let back: string = `!!`;
+
+
