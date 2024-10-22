24bf1aba37bd07d60f3e6d68e9861d4d747fe892
Charlie <158181191+Charlie2222223@users.noreply.github.com>
2024-10-22 13:56:21 +0900

    型注釈と型推論

--- TYPESCRIPT＿TYPE/index.ts
+++ TYPESCRIPT＿TYPE/index.ts
@@ -14,4 +14,16 @@ let single: string = 'hello';
 let double: string = "world";
 let back: string = `!!`;
 
+/**
+ * 型を宣言しなくても型推論で型を推測して割り当ててくれる
+ * 基本的には型推論で書いていく方がいい
+ */
+let hasvalue = true;
 
+/**
+ * 型推論ができない場合の例
+ */
+let hello;
+hello = 2;
+hello = "";
+hello = true;
