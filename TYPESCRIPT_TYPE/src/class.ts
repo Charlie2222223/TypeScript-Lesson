bfe4d4e663edb6ff35afee66f4a7ca71ebd9e65d
Charlie <158181191+Charlie2222223@users.noreply.github.com>
2024-10-26 12:42:19 +0900

    classを型にする方法

--- TYPESCRIPT_TYPE/src/class.ts
+++ TYPESCRIPT_TYPE/src/class.ts
@@ -10,17 +10,32 @@ class Person{
      * メソッドの作成
      * @param this thisを宣言することによりこのthisはどういうものなのかを示す　そうすることによりエラーを事前に防ぐことができる
      */
-    greetiong(this: {name: string}) {                  
+    // greetiong(this: {name: string}) {                  
+    //     console.log(`Hello!! My name is ${this.name}`);
+    // }
+
+    /**
+     * クラスを型にする方法
+     * @param this 
+     */
+    greeting(this: Person) {                  
         console.log(`Hello!! My name is ${this.name}`);
     }
 }
 
+let person3: Person;
+
 const quill = new Person('Quill');
-quill.greetiong();
+quill.greeting();
+
+// const anotherQuill = {
+//     name: 'anotherQuill',
+//     anotherGreeting: quill.greetiong
+// }
 
 const anotherQuill = {
     name: 'anotherQuill',
-    anotherGreeting: quill.greetiong
+    greeting: quill.greeting
 }
 
-anotherQuill.anotherGreeting();
+anotherQuill.greeting();
